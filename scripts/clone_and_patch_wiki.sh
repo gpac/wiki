d629fe3804f5f2ab1ed3d783a439b0af6beef119
nils <nils.dvl+git@gmail.com>
2024-02-24 06:39:29 +0100

    update wiki patches

--- scripts/clone_and_patch_wiki.sh
+++ scripts/clone_and_patch_wiki.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 git clone --depth 1 https://github.com/gpac/gpac.wiki.git ./docs
 cd $(pwd)/docs
-git apply --reject --whitespace=fix ../docs.diff
+git apply --reject --whitespace=fix ../patches/hotfix.diff
+git apply --reject --whitespace=fix ../patches/mkdocs-patch.diff
