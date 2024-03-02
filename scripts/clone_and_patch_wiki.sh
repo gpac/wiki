51297d3f7e65e3464df58d39f1ee1bccec13283b
nils <nils.dvl+git@gmail.com>
2024-03-02 09:49:36 +0100

    remove github wiki related scripts

--- scripts/clone_and_patch_wiki.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git clone --depth 1 https://github.com/gpac/gpac.wiki.git ./docs
-cd $(pwd)/docs
-git apply --reject --whitespace=fix ../patches/hotfix.diff
-git apply --reject --whitespace=fix ../patches/mkdocs-patch.diff
